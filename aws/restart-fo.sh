51b62af41e77cc6c663f6f4786eed892e9bb7933
116Lv <lbw0109@gmail.com>
2023-10-11 15:09:32 +0900

    20231011-4

---
+++ aws/restart-fo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 1
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+PROJECT_NAME=davogo-fo.war
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME})
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+
+# 2
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+# 3
+nohup java -jar ./${PROJECT_NAME} --spring.profiles.active=dev > /home/ec2-user/log/nohup_fo_log.out 2>&1 &
